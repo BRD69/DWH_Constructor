40b0e8fa4e0840fb656acdde624e6a4f59758fe0
BRD69 <46195542+BRD69@users.noreply.github.com>
2024-12-04 14:13:57 +0300

    Изменение файла сбрки

--- build.py
+++ build.py
@@ -4,7 +4,9 @@ PyInstaller.__main__.run([
     'main.py',
     '--onefile',
     '--windowed',
+    '--name=dwhc',
     '--icon=icon512.ico',
+    '--distpath=build',
 ])
 
 # PyInstaller.__main__.run(['main.spec'])
